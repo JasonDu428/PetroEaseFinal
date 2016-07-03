3647f3a9fea0f164ddb2da93a5cce917305a0e85
Jason Du <xindu@usc.edu>
2016-07-02 21:15:42 -0700

    added jquery motion control

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@
 
 // targets collapse bar. it collapses when when blur outside of the screen
 $(function () { // Same as document.addEventListener("DOMContentLoaded"...
-
   // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
   $("#navbarToggle").blur(function (event) {
     var screenWidth = window.innerWidth;
@@ -11,3 +10,32 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
     }
   });
 });
+
+
+// starting from here is bottom slid in
+var $animation_elements = $('.animation-element');
+var $window = $(window);
+
+function check_if_in_view() {
+  var window_height = $window.height();
+  var window_top_position = $window.scrollTop();
+  var window_bottom_position = (window_top_position + window_height);
+
+  $.each($animation_elements, function() {
+    var $element = $(this);
+    var element_height = $element.outerHeight();
+    var element_top_position = $element.offset().top;
+    var element_bottom_position = (element_top_position + element_height);
+
+    //check to see if this current container is within viewport
+    if ((element_bottom_position >= window_top_position) &&
+      (element_top_position <= window_bottom_position)) {
+      $element.addClass('in-view');
+    } else {
+      $element.removeClass('in-view');
+    }
+  });
+}
+
+$window.on('scroll resize', check_if_in_view);
+$window.trigger('scroll');
